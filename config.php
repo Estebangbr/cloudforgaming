a389add4bfcd87db9fc023c490e8639888b87db4
Esteban <gaboriau.est@gmail.com>
2022-06-29 10:32:24 +0200

    edit config.php for server

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
     try
     {
-        $bdd = new PDO("mysql:host=localhost;dbname=cloudforgaming;charset=utf8", "root", "");
+        $bdd = new PDO("mysql:host=c4g-database:3306;dbname=cloudforgaming;charset=utf8", "admin", "ESGIProjet123!");
     }
     catch(PDOException $e)
     {
